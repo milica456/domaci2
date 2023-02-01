66aa745491af9bfb8bc0da90b0d1df520d6ac70f
Milica Jaksic <mj20190209@student.fon.bg.ac.rs>
2023-02-01 13:23:36 +0100

    Napravljena laravel aplikacija

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      // $this->call(UserTableSeeder::class);
+    // $this->call(SektoriTableSeeder::class);
+      // $this->call(PozicijeTableSeeder::class);
+      // $this->call(ZaposleniTableSeeder::class);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('login', [UserController::class, 'login']);
+Route::post('register', [UserController::class, 'register']);
+
+Route::middleware('auth:sanctum')->group( function () {
+    Route::resource('sektori', \App\Http\Controllers\SektorController::class);
+    Route::resource('pozicije', \App\Http\Controllers\PozicijaController::class);
+    Route::resource('zaposleni', \App\Http\Controllers\ZaposleniController::class);
+});*/
